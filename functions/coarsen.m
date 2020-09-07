21428106f77bddf078a5414a5e5b26311326fc44
Craig Maxwell <hello@craigmaxwell.me>
2020-09-07 17:38:04 +0200

    Update coarsen.m
    % 22/06/2020 - improved readability
    % 22/06/2020 - removed errata with idea of randomising building flammability
    % 22/07/2020 - Implemented option for coarsening using coarsening factor
    % 22/07/2020 - Added description
    % 10/08/2020 - Separated coarsening and coarsen factor calculation

--- functions/coarsen.m
+++ functions/coarsen.m
@@ -1,4 +1,10 @@
 %% Coarsen raster grid
+% Generate coarsened occupancy map and raster map based on input map
+% Inputs:
+% - m_p_in - input map
+% - ref - (optional, requires l_d) raster reference map
+% - l_d - (optional, requires ref) reference cell size
+% - c_f - (optional) coarsening factor
 % m_occ - occupancy map (percent of each cell occupied)
 % m_r   - raster map (binary, identifies if any occupancy in cell)
 
@@ -7,56 +13,41 @@
 % of matrix.
 
 %% To do
-% - check distance estimations accurate
-% - return coarsen factor
-% - fix problem of scan map and environment map cell sizes not being an integer
-% factor of each other
-% - must be a better way of managing the environment map
 
 %% Change log
 % 22/06/2020 - improved readability
 % 22/06/2020 - removed errata with idea of randomising building flammability
+% 22/07/2020 - Implemented option for coarsening using coarsening factor
+% 22/07/2020 - Added description
+% 10/08/2020 - Separated coarsening and coarsen factor calculation
 
 %% Coarsen raster
-function [m_occ, m_r, l_c_x, l_c_y] = coarsen(m_p_in, ref, l_d)
-    
-    % Difference in lat negligible for small scale
-    lat = ref.LatitudeLimits(1);
-    m_per_deg_lat = 111132.954 - 559.822 * cos( 2 * lat ) + 1.175 * cos( 4 * lat);
-    m_per_deg_lon = 111132.954 * cos ( lat );
-    l_r_lat = ref.CellExtentInLatitude * m_per_deg_lat;
-    l_r_lon = ref.CellExtentInLongitude * m_per_deg_lon;
-    
-    % Coarsen to desired cell size - side to right is removed if necessary
-    n_lat   = round(l_d/l_r_lat);
-    n_lon   = round(l_d/l_r_lon);
-    % length of new cells in x direction
-    l_c_x   = n_lat * m_per_deg_lat * ref.CellExtentInLatitude;  
-    % length of new cells in y direction
-    l_c_y   = n_lon * m_per_deg_lon * ref.CellExtentInLongitude;
-    
-    g_d     = [floor(size(m_p_in,1)/n_lat), floor(size(m_p_in,2)/n_lon)];
-    m_occ   = zeros(g_d(1), g_d(2));
-    m_r     = zeros(g_d(1), g_d(2));
-       
-    if n_lat < 2 || n_lon < 2
-        error("Coarsen ratio not high enough")
-    end
+function [m_occ, m_r] = coarsen(m_p_in, c_f)
 
-    for i = 1:g_d(1)
-        for j = 1:g_d(2)
-            ii = (i-1)*n_lat+1;
-            jj = (j-1)*n_lon+1;        
-            mat = m_p_in(ii:ii+n_lat-1,jj:jj+n_lon-1); % Extract data from original raster
-            occ = nnz(mat)/(n_lat*n_lon);
-            
-            % Building occupancy ratio
-            m_occ(i,j) = occ;
-            
-            % Building flammability
-            if occ > 0 
-                m_r(i,j) = 1;
-            end
-        end
+  n_lat = c_f(1);
+  n_lon = c_f(2);
+  
+  % Initialise maps
+  g_d     = [floor(size(m_p_in,1)/n_lat), floor(size(m_p_in,2)/n_lon)];
+  m_occ   = zeros(g_d(1), g_d(2));
+  m_r     = zeros(g_d(1), g_d(2));
+
+  % Calculate occupancy map and raster map
+  for i = 1:g_d(1)
+    for j = 1:g_d(2)
+      ii = (i-1)*n_lat+1;
+      jj = (j-1)*n_lon+1;        
+      % Extract data from original raster
+      mat = m_p_in(ii:ii+n_lat-1,jj:jj+n_lon-1); 
+      occ = sum(mat, 'all')/(n_lat*n_lon);
+
+      % occupancy map
+      m_occ(i,j) = occ;
+
+      % raster map
+      if occ > 0 
+        m_r(i,j) = 1;
+      end
     end
+  end
 end
