7ad8524d2eb6e1277edcd10d7a4ec457a170030f
Craig Maxwell <hello@craigmaxwell.me>
2020-09-07 17:28:31 +0200

    Update uavModel.m
    % 22/06/2020 - reworked UAV task assignment
    % 22/06/2020 - corrected UAV distance calculations
    % 22/06/2020 - improved readability

--- functions/uavModel.m
+++ functions/uavModel.m
@@ -4,20 +4,21 @@
 
 % To do:
 % Improve calculation of agent times - linear so can increase step size
-% Queue should always ensure that time to complete is greater than
-% control timestep size!
+% Problem when timestep is too large and multiple actions can be carried out?
+% What happens if UAV task = 3, unable to do any other task?
 
 %% Change Log
 % 22/06/2020 - reworked UAV task assignment
 % 22/06/2020 - corrected UAV distance calculations
 % 22/06/2020 - improved readability
 
-function [  m_scan, m_scan_hist, m_scan_t_hist, ...
-            UAV_loc, UAV_loc_hist, UAV_task, UAV_target, ...
+%% Bugs
+% - UAVs beging assigned cells outside of search range
+
+function [  m_s, UAV_loc, UAV_loc_hist, UAV_task, UAV_target, ...
             t_travel_UAV, t_scan_UAV] ...
             = uavModel( n_UAV, ...
-                m_t_scan, m_scan, m_scan_hist, m_scan_t_hist, ...
-                UAV_loc, UAV_loc_hist, UAV_task, UAV_target, ...
+                m_t_scan, m_s, UAV_loc, UAV_loc_hist, UAV_task, UAV_target, ...
                 t_travel_UAV, t_scan_UAV, ...
                 l_c_s_x, l_c_s_y, v_as_UAV, v_w, ang_w, dt_a, t)
 
@@ -29,47 +30,37 @@ function [  m_scan, m_scan_hist, m_scan_t_hist, ...
         % Set task to scan
         UAV_task(UAV)   = 2;                            
         % Update UAV location
-        UAV_loc(UAV, :) = UAV_target(UAV, :, 1);        
+        UAV_loc(UAV, :) = UAV_target(UAV, :, 1); 
+        % Update UAV location history
         UAV_loc_hist    = [UAV_loc_hist; UAV_loc(UAV, :), UAV, t];        
         % Initialise remaining scantime
         t_scan_UAV(UAV) = m_t_scan(UAV_loc(UAV, 1), UAV_loc(UAV, 2)) + t_travel_UAV(UAV);
       end
-    else
+    elseif UAV_task(UAV) == 2
       % Scan
       t_scan_UAV(UAV) = t_scan_UAV(UAV) - dt_a;
       if t_scan_UAV(UAV) <= 0
         % Set task to travel
         UAV_task(UAV) = 1;
         % Set cell to scanned
-        m_scan(UAV_loc(UAV, 1), UAV_loc(UAV, 2)) = 1; 
-        % Update scan map history
-        m_scan_hist   = [m_scan_hist; m_scan];        
-        % Update scan map time history
-        m_scan_t_hist = [m_scan_t_hist; t];
+        m_s(UAV_loc(UAV, 1), UAV_loc(UAV, 2)) = 1; 
         
-        % Assign targets
+        % Shift target list
         UAV_target(UAV, 1, :)   = circshift(UAV_target(UAV, 1, :), -1);
         UAV_target(UAV, 2, :)   = circshift(UAV_target(UAV, 2, :), -1);        
         UAV_target(UAV, :, end) = [NaN, NaN];
-        
-        % Start location
-        loc_1 = UAV_loc(UAV, :);  
-        % End location
-        loc_2 = UAV_target(UAV, :, 1);                  
-        % Distance in m
-        dist        = (sqrt ((l_c_s_x*(loc_2(1) - loc_1(1))).^2) ...
-                    +       ((l_c_s_y*(loc_2(2) - loc_1(2))).^2));
-        % Ground angle
-        ang_g       = atan2(loc_1(2) - loc_2(2), loc_1(1) - loc_2(1));                % Ground angle
-        % Wind to track angle
-        a_wt        = ang_g - ang_w;
-        % Wind correction angle
-        ang_wca     = asin(v_w*sin(a_wt)/v_as_UAV);
-        % Ground speed
-        v_gs        = v_as_UAV*cos(ang_wca) + v_w*cos(a_wt);                
-        % Add to travel time
-        t_travel_UAV(UAV) = dist/v_gs;
+       
+        if(isnan(UAV_target(UAV, :, 1)))
+          % Set task to idle
+          UAV_task(UAV) = 3;
+        else
+          % Calculate travel time
+          t_travel_UAV(UAV) = travelTime(UAV_loc(UAV, :), UAV_target(UAV, :, 1), ...
+            l_c_s_x, l_c_s_y, ang_w, v_w, v_as_UAV);
+        end
       end
+    elseif (UAV_task == 3)
+      return
     end
   end
 end
