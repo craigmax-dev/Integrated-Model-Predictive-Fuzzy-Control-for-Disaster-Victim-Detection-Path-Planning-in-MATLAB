2548561d233561f0fb3387cbfa16b95aa91b7ee7
Craig Maxwell <hello@craigmaxwell.me>
2020-09-30 16:02:58 +0200

    Initial work on plotting functions

---
+++ functions/func_plot_labels.m
@@ -0,0 +1,18 @@
+function [lab_title, lab_x, lab_y, lab_legend] = func_plot_labels(data_name)
+  if data_name == "m_f_hist_animate"
+    lab_title = "";
+    lab_x = "Latitude";
+    lab_y = "Longitude";
+    lab_legend = "";
+  elseif data_name == "m_dw_hist_animate"
+    lab_title = ;
+    lab_x = "Latitude";
+    lab_y = "Longitude";
+    lab_legend = "";
+   elseif data_name == "m_scan_hist_animate"
+    lab_title = ;
+    lab_x = "Latitude";
+    lab_y = "Longitude";
+    lab_legend = "";
+  end    
+end
