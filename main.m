d75fa49fcbcddc505525e3a39d817ee6dc60afca
Craig Maxwell <hello@craigmaxwell.me>
2020-10-29 07:07:01 +0100

    Results - simulation 7

--- main.m
+++ main.m
@@ -94,10 +94,13 @@ h_init_MPC_SOLV_patternsearch = @(fisArray, n_a)initialise_MPC_ST01_patternsearc
 %   "SS05-4", h_init_sim_1, h_init_env_1, h_init_agt_1, h_init_pp_2, h_init_MPC_maxfunceval_100;
 %   };
 % % Simulation set 6 - n_MF_out = 2, n_p = 2
-simulation_set_name = "SS06";
-simulation_set = {
-  "SS06-4", h_init_sim_1, h_init_env_1, h_init_agt_1, h_init_pp_2, h_init_MPC_maxfunceval_100_2;
-  };
+% simulation_set_name = "SS06";
+% simulation_set = {
+%   "SS06-1", h_init_sim_1, h_init_env_1, h_init_agt_1, h_init_pp_2, h_init_MPC_1;
+%   "SS06-2", h_init_sim_1, h_init_env_1, h_init_agt_1, h_init_pp_2, h_init_MPC_maxfunceval_10_2;
+%   "SS06-3", h_init_sim_1, h_init_env_1, h_init_agt_1, h_init_pp_2, h_init_MPC_maxfunceval_40_2;
+%   "SS06-4", h_init_sim_1, h_init_env_1, h_init_agt_1, h_init_pp_2, h_init_MPC_maxfunceval_100_2;
+%   };
 % % Simulation set 7 - n_MF_out = 2, n_p = 3
 % simulation_set_name = "SS07";
 % simulation_set = {
